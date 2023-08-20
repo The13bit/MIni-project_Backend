c53fb3209993373d25045d5c71cd90d58f8d4ac5
The13bit <anasfodkar5@gmail.com>
2023-08-20 19:05:49 +0530

    implemented download and upgraded data processing

--- Server/Routes/api-data.js
+++ Server/Routes/api-data.js
@@ -7,10 +7,24 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const storage=multer.diskStorage({
+
+    destination:function(req,file,cb){
+        cb(null,'uploads/');
+    },
+    filename:function(req,file,cb){
+        const originalname=path.parse(file.originalname).name;
+        //const uniqesuffix=Date.now()+'-';   //for unique file name
+        const customfilename=`${req.body.year}-${req.body.sem}-${req.body.branch}-${originalname}${path.extname(file.originalname)}`;
+        cb(null,customfilename);
+    }
+
+});
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ storage: storage });
 
+//main handel route
 router.post('/upload',upload.single('file'), (req, res) => {
 const response={
     year:req.body.year,
@@ -22,7 +36,7 @@ const filePath=req.file.path;
 console.log(filePath);
 //res.send("yesssss"+ JSON.stringify(response));
 
-const pythonprocess=spawn('python',[path.join(__dirname,'../Scripts/script.py'),filePath]);
+const pythonprocess=spawn('python',[path.join(__dirname,'../Scripts/script.py'),filePath,response.year,response.sem,response.branch]);
 
 pythonprocess.stdout.on('data',(data)=>{
     console.log(data.toString());
@@ -45,6 +59,30 @@ pythonprocess.on('close',(code)=>{
 
 router.get('/login', (req, res) => {
     console.log('yes');
+});
+
+
+//get file route
+router.get('/download/:year/:sem/:branch', (req, res) => {
+    console.log('hit')
+    const year=req.params.year;
+    const sem=req.params.sem;
+    const branch=req.params.branch;
+
+    const folderpath=path.join(__dirname,'../processed_data')
+    res.download(folderpath+`/${year}_${sem}_${branch}.csv`)
+   
+     
+
+
+
+
+});
+
+//test router to check if button works
+router.get('/test', (req, res) => {
+    console.log('hit')
+    res.send('test');
 });
 
 
--- Server/Scripts/script.py
+++ Server/Scripts/script.py
@@ -1,7 +1,17 @@
 import pandas as pd
 import numpy as np
 import sys
+import os
 url=sys.argv[1]
+year=sys.argv[2]
+sem=sys.argv[3]
+branch=sys.argv[4]
+
+folder_path='processed_data/'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+file_path=os.path.join(folder_path,year+'_'+sem+'_'+branch+'.csv')
+
 df=pd.read_csv(url)
 
 
@@ -9,5 +19,6 @@ df=pd.read_csv(url)
 df.set_index('Std_Branch', inplace=True)
 grouped=df.groupby('Std_Branch')['Std_Marks'].mean()
 grouped=grouped.reset_index()
+grouped.to_csv(file_path, index=False)
 print(grouped)
 
